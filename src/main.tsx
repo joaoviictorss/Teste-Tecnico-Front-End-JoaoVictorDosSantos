ae0c167f941d5573542b028cf048ec69fadaf830
João Victor dos Santos Silva <137708191+joaoviictorss@users.noreply.github.com>
2024-06-29 11:50:21 -0300

    adiciona o providers do react hot toast

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import ToastProvider from "./providers/toast-provider.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { makeServer } from "./server.ts";
+
 import Header from "./components/header/index.tsx";
 import Footer from "./components/footer/index.tsx";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Cart from "./components/cart/index.tsx";
 
 // Rotas da aplicação com o react router
@@ -26,6 +27,7 @@ makeServer();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ToastProvider />
     <Header />
     <RouterProvider router={router} />
     <Footer />
